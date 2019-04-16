d8a4a0b926b8e0b6132fde2d15297c75f7d8ad4b
Joel Palmer <joelpalmer@gmail.com>
2019-04-16 14:44:42 -0500

    update temps

--- temps.c
+++ temps.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 main() {
-    printf("Joel");
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
 }
